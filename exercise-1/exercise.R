83d8cc69a911324c10a536cd88dedf8d438aa6ac
Tiffany Truong <melissatruong@D-173-250-143-205.dhcp4.washington.edu>
2018-04-04 16:10:43 -0700

    Call built-in functions

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -26,8 +26,8 @@ fav_2 <- 4
 
 # Divide each number by the square root of 201 and save the new value in the
 # original variable
-fav_1 <- fav_1/sqrt(201)
-fav_2 <- fav_2/sqrt(201)
+fav_1 <- fav_1 / sqrt(201)
+fav_2 <- fav_2 / sqrt(201)
 
 # Create a variable `raw_sum` that is the sum of the two variables. Use the
 # `sum()` function for practice.
@@ -43,7 +43,7 @@ round_1 <- round(fav_1, 1)
 round_2 <- round(fav_2, 1)
 
 # Create a variable `sum_round` that is the sum of the rounded values
-sum_round <- round(round_1, round_2)
+sum_round <- sum(round_1, round_2)
 
 # Which is bigger, `round_sum` or `sum_round`? (You can use the `max()` function!)
 max(round_sum, sum_round)
